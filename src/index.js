eec70ebf291d465437ce74200ecbf5b1b2ff04ab
carlosavellar <carlos.avellar@gmail.com>
2021-09-29 11:01:17 -0300

    feat: Cadastro intrerface

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** @format */
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { ThemeProvider } from "@mui/material/styles";
+// import theme from "./assets/theme";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Cadastro from "./components/cadastro/";
+import Repositorios from "./components/respositorios";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Cadastro} />
+        <Route path="/respositorios" component={Repositorios} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
